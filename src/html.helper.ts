e0076e6b09cf6c402e339b93236cea4346347343
roksilic <roksilic.net@gmail.com>
2024-04-19 15:03:11 +0200

    Page class for mapping pages into object

---
+++ src/html.helper.ts
@@ -0,0 +1,55 @@
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+import * as path from "path";
+import { downloadImage, generateUrl } from "./scrape.service";
+
+export class Page {
+  html: any;
+  pagesDirectory = path.join("src", "pages");
+  imagesDirectory = path.join("src", "images");
+
+  constructor(pageContent: string) {
+    this.html = cheerio.load(pageContent);
+  }
+
+  savePage(pageContent: string, fileName: string): void {
+    fs.writeFileSync(path.join(this.pagesDirectory, fileName), pageContent);
+  }
+
+  async downloadImages(): Promise<void> {
+    // Query for all <img></img>
+    const images = this.html("img");
+
+    console.log("Starting downloading images");
+
+    for (const image of images) {
+      const imageUrl = this.html(image).attr("src").replace("..", "") as string;
+      await downloadImage(
+        generateUrl(imageUrl),
+        path.join(this.imagesDirectory, imageUrl.replace(/\//g, "_"))
+      );
+    }
+
+    console.log(`Ended image download, downloaded: ${images.length} images`);
+  }
+
+  hasNextPage(): boolean {
+    return this.html("div ul.pager li.next").length > 0;
+  }
+
+  getNextPageUrl(): string {
+    const urlParam = this.html("div ul.pager li.next a").attr("href");
+
+    if (urlParam.split("/").length > 1) {
+      return urlParam;
+    }
+
+    return `catalogue/${urlParam}`;
+  }
+
+  getNumberOfPage(): number {
+    const urlParam = this.getNextPageUrl();
+
+    return Number(urlParam.split("/")[1].split(".")[0].split("-")[1]) - 1;
+  }
+}

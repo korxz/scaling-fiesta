fa057236ceb6882bbb3f1c3ccfcc6a3ef6a11424
roksilic <roksilic.net@gmail.com>
2024-04-23 09:49:15 +0200

    Removed loadNextPage function

--- src/scrape.service.ts
+++ src/scrape.service.ts
@@ -4,40 +4,6 @@ import { baseUrl } from "./constants";
 import * as fs from "fs";
 import * as https from "https";
 
-export async function loadNextPage(
-  url: string,
-  numberOfPage: number
-): Promise<void> {
-  numberOfPage++;
-
-  const response = await axios.get(`${baseUrl}/${url}`);
-
-  const page = new Page(response.data);
-
-  const fileName = url.split("/")[1];
-
-  console.log(`Saving page number: ${numberOfPage}`);
-
-  page.savePage(response.data, fileName);
-  await page.downloadImages();
-
-  const hasNextPage = page.hasNextPage();
-
-  if (hasNextPage) {
-    await loadNextPage(page.getNextPageUrl(), numberOfPage);
-  }
-
-  return;
-}
-
-export function generateUrl(urlPath: string): string {
-  if (urlPath.startsWith("/")) {
-    return `${baseUrl}${urlPath}`;
-  }
-
-  return `${baseUrl}/${urlPath}`;
-}
-
 export async function downloadImage(
   url: string,
   filename: string

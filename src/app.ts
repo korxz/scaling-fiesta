d48e32af3ee1477f09779041a1e8b67b237805e2
roksilic <roksilic.net@gmail.com>
2024-04-23 09:46:02 +0200

    Reworked entry point to use while loop instead of rec.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Page } from "./html.helper";
 import { baseUrl } from "./constants";
-import { loadNextPage } from "./scrape.service";
+import sanitize from "sanitize-filename";
 
 (async () => {
   console.log("Program started.");
@@ -12,17 +12,51 @@ import { loadNextPage } from "./scrape.service";
 
   const page = new Page(response.data);
 
-  console.log("Saving page number: 1");
+  console.log(`Saving a page on url: ${baseUrl}`);
 
-  page.savePage(response.data, "homepage.html");
+  page.savePage(response.data, `${sanitize(baseUrl)}.html`);
 
-  await page.downloadImages();
+  const scrapedPages: {
+    [key: string]: boolean;
+  } = {};
 
-  let hasNextPage = page.hasNextPage();
-  let nextPageUrl = page.getNextPageUrl();
+  const pagesToSrap: { [key: string]: boolean } = {};
 
-  if (hasNextPage) {
-    await loadNextPage(nextPageUrl, 1);
+  for (const link of page.getPageLinks(baseUrl)) {
+    pagesToSrap[link] = true;
+  }
+
+  scrapedPages[baseUrl] = true;
+
+  await page.downloadImages(baseUrl);
+
+  while (Object.keys(pagesToSrap).length > 0) {
+    const keys = Object.keys(pagesToSrap);
+    const url = keys[0];
+
+    const response = await axios.get(url);
+
+    const page = new Page(response.data);
+
+    console.log(`Saving a page on url: ${url}`);
+
+    page.savePage(response.data, `${sanitize(url)}.html`);
+
+    scrapedPages[url] = true;
+    await page.downloadImages(url);
+
+    const otherPageLinks: Array<string> = page.getPageLinks(url);
+
+    for (const pageLink of otherPageLinks) {
+      if (
+        scrapedPages[pageLink] === undefined &&
+        pagesToSrap[pageLink] === undefined
+      ) {
+        pagesToSrap[pageLink] = true;
+      }
+    }
+    console.log(`Number of pages to scrap: ${Object.keys(pagesToSrap).length}`);
+    delete pagesToSrap[url];
   }
 
   console.log("Program ended.");

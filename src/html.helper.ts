4e58c75df2b79744172f39902ebfcc9ac794661e
roksilic <roksilic.net@gmail.com>
2024-04-23 10:33:15 +0200

    Renamed cheerio load property to $ and added html content to data

--- src/html.helper.ts
+++ src/html.helper.ts
@@ -4,51 +4,56 @@ import * as path from "path";
 import { downloadImage } from "./scrape.service";
 
 export class Page {
-  html: any;
+  $: cheerio.CheerioAPI;
+  html: string;
   pagesDirectory = path.join("src", "pages");
   imagesDirectory = path.join("src", "images");
 
-  constructor(pageContent: string) {
-    this.html = cheerio.load(pageContent, { xmlMode: true });
+  constructor(data: string) {
+    this.html = data;
+    this.$ = cheerio.load(data, { xmlMode: true });
   }
 
-  savePage(pageContent: string, fileName: string): void {
-    fs.writeFileSync(path.join(this.pagesDirectory, fileName), pageContent);
+  savePage(fileName: string): void {
+    fs.writeFileSync(path.join(this.pagesDirectory, fileName), this.html);
   }
 
   async downloadImages(baseUrl: string): Promise<void> {
     // Query for all <img></img>
-    const images = this.html("img");
+    const images = this.$("img");
 
     console.log("Starting downloading images");
 
     for (const image of images) {
-      const pathUrl = this.html(image).attr("src");
-      const url = new URL(pathUrl, baseUrl).href;
+      const pathUrl = this.$(image).attr("src");
 
-      await downloadImage(
-        url,
-        path.join(this.imagesDirectory, url.replace(/\//g, "_"))
-      );
+      if (pathUrl !== undefined) {
+        const url = new URL(pathUrl, baseUrl).href;
+
+        await downloadImage(
+          url,
+          path.join(this.imagesDirectory, url.replace(/\//g, "_"))
+        );
+      }
     }
 
     console.log(`Ended image download, downloaded: ${images.length} images`);
   }
 
   getPageLinks(url: string): Array<string> {
-    const links = this.html("a");
+    const links = this.$("a");
 
     console.log("Scrapping <a></a> tags on page.");
 
     const hrefs: Array<string> = [];
 
     // Exclude previous page
-    const previousPage = this.html("link.previous a").attr("href");
+    const previousPage = this.$("link.previous a").attr("href");
 
     for (const link of links) {
-      const href = this.html(link).attr("href");
+      const href = this.$(link).attr("href");
 
-      if (href !== previousPage) {
+      if (href !== previousPage && href !== undefined) {
         // Add to arrays, already scrapped and yet to scrape so we prevent looping
         const absoluteUrl = new URL(href, url).href;
         hrefs.push(absoluteUrl);
